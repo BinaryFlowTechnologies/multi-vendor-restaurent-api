7938a9e3b0f556ba0f4bf78fe3dd3d93e544d3eb
Sukanta Purkayastha <spurkayastha83@gmail.com>
2024-08-30 23:51:14 +1000

    restaurent factory added

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Restaurant;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -17,10 +18,11 @@ class CategoryFactory extends Factory
         $categoryName = $this->faker->streetName;
 
         return [
-            'name'   => $categoryName,
-            'slug'   => Str::slug($categoryName),
-            'sort'   => $this->faker->numberBetween(0, 100),
+            'name' => $categoryName,
+            'slug' => Str::slug($categoryName),
+            'sort' => $this->faker->numberBetween(0, 100),
             'status' => $this->faker->randomElement([true, false]),
+            'restaurant_id' => $this->faker->randomElement(Restaurant::all()->pluck('id')->toArray()),
         ];
 
     }
